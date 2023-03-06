a3c656c64cbec3228e2322569826a91d162e6aa5
cskeith <cslklee@gmail.com>
2023-03-06 17:48:00 +0800

    Lab 7 Task 4 completed

--- src/main/java/hkmu/comps380f/MyController.java
+++ src/main/java/hkmu/comps380f/MyController.java
@@ -8,10 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Controller
 public class MyController {
+
+    private final Map<Integer, String> products = new ConcurrentHashMap<>();
+
+    public MyController() {
+        this.products.put(1, "Sandpaper");
+        this.products.put(2, "Nails");
+        this.products.put(3, "Glue");
+        this.products.put(4, "Paint");
+        this.products.put(5, "Tape");
+    }
 
     @GetMapping("/")
     public String index() {
@@ -53,4 +65,61 @@ public class MyController {
         visits.add(now);
         return "viewSessionActivity";
     }
+
+    @GetMapping("/shop")
+    public String shop(HttpServletRequest request, HttpSession session) {
+        String action = request.getParameter("action");
+        if (action == null)
+            action = "browse";
+
+        switch (action) {
+            case "addToCart":
+                return this.addToCart(request, session);
+            case "viewCart":
+                return this.viewCart(request);
+            case "emptyCart":
+                return this.emptyCart(session);
+            case "browse":
+            default:
+                return this.browse(request);
+        }
+    }
+
+    // Defining other methods ...
+    private String viewCart(HttpServletRequest request) {
+        request.setAttribute("products", this.products);
+        return "viewCart";
+    }
+
+    private String browse(HttpServletRequest request) {
+        request.setAttribute("products", this.products);
+        return "browse";
+    }
+
+    private String addToCart(HttpServletRequest request, HttpSession session) {
+        int productId;
+        try {
+            productId = Integer.parseInt(request.getParameter("productId"));
+        } catch (Exception e) {
+            return "redirect:/shop";
+        }
+
+        if (session.getAttribute("cart") == null)
+            session.setAttribute("cart", new ConcurrentHashMap<>());
+
+        @SuppressWarnings("unchecked")
+        Map<Integer, Integer> cart
+                = (Map<Integer, Integer>) session.getAttribute("cart");
+        if (!cart.containsKey(productId))
+            cart.put(productId, 0);
+        cart.put(productId, cart.get(productId) + 1);
+
+        return "redirect:/shop?action=viewCart";
+    }
+
+    private String emptyCart(HttpSession session) {
+        session.removeAttribute("cart");
+        return "redirect:/shop?action=viewCart";
+    }
+
 }
