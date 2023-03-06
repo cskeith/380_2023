30e304c4e3962452c5f63a8f7acd70cb2133aefd
cskeith <cslklee@gmail.com>
2023-03-06 17:16:55 +0800

    Lab 7 Task 2 completed

--- src/main/java/hkmu/comps380f/MyController.java
+++ src/main/java/hkmu/comps380f/MyController.java
@@ -2,6 +2,7 @@ package hkmu.comps380f;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MyController {
@@ -10,4 +11,15 @@ public class MyController {
     public String index() {
         return "viewSessionActivity";
     }
+
+    @GetMapping("/checkboxes")
+    public String multiValueForm() {
+        return "MultiValueForm";
+    }
+
+    @PostMapping("/checkboxes")
+    public String multiValueResult() {
+        return "MultiValueResult";
+    }
+
 }
