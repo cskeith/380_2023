e0e9b9579a21c323ccbedddf4dc0d50696976424
Lap-Kei Lee <cslklee@gmail.com>
2023-03-10 23:43:06 +0800

    Lecture 7 HelloSpringMVC example

---
+++ src/main/java/hkmu/comps380f/GuestBookEntry.java
@@ -0,0 +1,42 @@
+package hkmu.comps380f;
+
+import java.util.Date;
+
+public class GuestBookEntry {
+    Integer id;
+    String name;
+    String message;
+    Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
