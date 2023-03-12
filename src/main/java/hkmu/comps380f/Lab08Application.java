d55739dcce3202682391efd27c0aa06fb91e0001
Lap-Kei Lee <cslklee@gmail.com>
2023-03-12 17:06:39 +0800

    Lab 8 Spring Boot project

---
+++ src/main/java/hkmu/comps380f/Lab08Application.java
@@ -0,0 +1,13 @@
+package hkmu.comps380f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab08Application.class, args);
+    }
+
+}
